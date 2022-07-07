65c72d144acfcb81a3bc44a96aa1646493db2f75
JM Gustafson <jodymgustafson@hotmail.com>
2022-07-06 21:41:08 -0400

    oop

--- 1.oop/0-interfaces.ts
+++ 1.oop/0-interfaces.ts
@@ -17,14 +17,6 @@ export interface IPoint3D extends IPoint2D {
 export interface IShape {
     name: string;
     numberOfSides: number;
-    // Use a ? to specify optional fields
-    points?: IPoint2D[];
-    getArea?: () => number;
+    points: IPoint2D[];
+    getArea(): number;
 }
-
-const shapes: IShape[] = [];
-
-shapes.push({
-    name: "triangle",
-    numberOfSides: 3
-});
--- 1.oop/1-classes.ts
+++ 1.oop/1-classes.ts
@@ -1,47 +1,105 @@
-﻿import { IPoint2D } from "./0-interfaces";
-
-// Use implements to implement an interface
-
-export class Point2D implements IPoint2D {
-    // Note: default access is public
-    x: number;
-    y: number;
-
-    constructor(x: number, y: number) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public toString(): string {
-        return `Point: ${this.x}, ${this.y}`; // template literal
-    }
-
-}
-
-const p1 = new Point2D(0, 0);
-const p2 = new Point2D(10, 10);
-console.log(p1.toString());
+﻿import { IPoint2D, IShape } from "./0-interfaces";
 
 //----------------------------------------~*~----------------------------------------//
-// Use extends to extend an interface
+// Note: default access for class members is public
 
-interface IPoint3D extends IPoint2D {
-    z: number;
-}
+export abstract class Shape implements IShape {
+    private _name: string;
+    private _numberOfSides: number;
+    protected _points?: IPoint2D[];
 
-class Point3D extends Point2D implements IPoint3D {
-    public z: number;
-
-    constructor(x: number, y: number, z: number) {
-        super(x, y);
-        this.z = z;
+    constructor(name: string, numberOfSides: number, points: IPoint2D[]) {
+        this._name = name;
+        this._numberOfSides = numberOfSides;
+        this._points = points;
     }
 
-    // @override
-    public toString(): string {
-        return `${super.toString()}, ${this.z}`;
+    get name(): string {
+        return this._name;
+    }
+
+    get numberOfSides(): number {
+        return this._numberOfSides;
+    }
+
+    get points(): IPoint2D[] {
+        return this._points;
+    }
+    set points(p: IPoint2D[]) {
+        this._points = p;
+    }
+
+    abstract getArea(): number;
+
+    translate(dx: number, dy: number): void {
+        for (const p of this.points) {
+            p.x += dx;
+            p.y += dy;
+        }
+    }
+
+    toString(): string {
+        return this.name;
     }
 }
 
-const p3 = new Point3D(10, 20, 30);
-console.log(p3.toString());
+export class Triangle extends Shape {
+    constructor(points: [IPoint2D, IPoint2D, IPoint2D]) {
+        super("triangle", 3, points);
+    }
+
+    getArea(): number {
+        const p1 = this.points[0];
+        const p2 = this.points[1];
+        const p3 = this.points[2];
+        return .5 * Math.abs(p1.x * (p2.y - p3.y) + p2.x * (p3.y - p1.y) + p3.x * (p1.y - p2.y))
+    }
+}
+
+export class Rectangle extends Shape {
+    constructor(points: [IPoint2D, IPoint2D, IPoint2D, IPoint2D]) {
+        super("rectangle", 4, points);
+    }
+
+    get width(): number {
+        return Math.abs(this.points[0].x - this.points[2].x)
+    }
+
+    get height(): number {
+        return Math.abs(this.points[0].y - this.points[2].y);
+    }
+
+    getArea(): number {
+        return this.width * this.height;
+    }
+
+    isSquare(): boolean {
+        return this.width === this.height;
+    }
+
+    /** @override */
+    toString(): string {
+        return `${super.toString()} (${this.width} x ${this.height})`;
+    }
+}
+
+const shapes: IShape[] = [];
+
+shapes.push(new Triangle([
+    { x: 0, y: 0},
+    { x: 0, y: 2},
+    { x: 3, y: 0},
+]));
+
+shapes.push(new Rectangle([
+    { x: 0, y: 0},
+    { x: 0, y: 2},
+    { x: 3, y: 2},
+    { x: 3, y: 0}
+]));
+
+console.log(shapes[0].toString());
+console.log(shapes[0].getArea());
+console.log(shapes[1].toString());
+console.log(shapes[1].getArea());
+console.log((shapes[1] as Rectangle).isSquare());
